b28c52880edde15b60d54022e79d5bb97a79302f
LARISSA CRISTINA GATTO GONCALVES <00001080852475sp@aluno.educacao.sp.gov.br>
2024-06-04 13:54:29 -0300

    atualizaçao html parte 3

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-alert("teste")
+function calcular(){
+    var fPeso = parseFloat(document.getElementById("peso"))
+    var fAltura = parseFloat(document.getElementById("altura"))
+    var fIMC = fPeso/(fAltura*fAltura)
+    
+}
