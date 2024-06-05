8dc7113a067143695d15714b0710a7b685c8aad5
LARISSA CRISTINA GATTO GONCALVES <00001080852475sp@aluno.educacao.sp.gov.br>
2024-06-05 14:41:42 -0300

    final dessa programção

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function calcular(){
-    var fPeso = parseFloat(document.getElementById("peso"))
-    var fAltura = parseFloat(document.getElementById("altura"))
-    var fIMC = fPeso/(fAltura*fAltura)
+    var fPeso = parseFloat(document.getElementById("peso").value)
     
+    var fAltura = parseFloat(document.getElementById("altura").value)
+    var fIMC = fPeso/(fAltura*fAltura)
+    document.getElementById("imc").innerHTML = fIMC.toFixed(2) 
+    if(fIMC<18.5){
+        document.getElementById("status").innerHTML = "STATUS: BAIXO PESO"   
+    }else if(fIMC<=24.9){
+        document.getElementById("status").innerHTML = "STATUS: PESO IDEAL"   
+    }else if(fIMC<=29.9){
+        document.getElementById("status").innerHTML = "STATUS: SOBREPESO"   
+    }else if(fIMC<=34.9){
+        document.getElementById("status").innerHTML = "STATUS: OBESIDADE 1"   
+    }else if(fIMC<=39.9){
+        document.getElementById("status").innerHTML = "STATUS: OBESIDADE 2"   
+    }else{
+        document.getElementById("status").innerHTML = "STATUS: OBESIDADE MORBIDA"
+
+    }
 }
